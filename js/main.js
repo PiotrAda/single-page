d63175c3d699361069f782fa82df819fe3c9b3c4
Piotr Adamus <adamusptr@gmail.com>
2017-04-21 06:16:56 +0200

    optimization

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ $(document).on('click', 'a', function(event){
   if(this.getAttribute("href").charAt(0) === "#") {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $($(this).attr('href')).offset().top
     }, 500);
   }
 });
